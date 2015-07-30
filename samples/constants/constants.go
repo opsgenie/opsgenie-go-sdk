ca82f934b963d18fd8c6c002af5371fd45bbda54
tubaozturk <tuba.ozturk@ifountain.com>
2015-07-30 10:33:53 +0300

    Constant variables comments.

--- samples/constants/constants.go
+++ samples/constants/constants.go
@@ -7,25 +7,25 @@ license that can be found in the LICENSE file.
 //Package constants provides common variables used in samples.
 package constants
 
-//Variable represents apiKey.
+//Variable represents OpsGenie apiKey.
 var APIKey = "YOUR API KEY HERE"
-//Variable represents apiKey.
+//Variable represents an OpsGenie Team.
 var TeamName = "YOUR TEAM NAME HERE"
-//Variable represents apiKey.
+//Variable represents an Owner of the OpsGenie Alert.
 var Owner = "YOUR USERNAME HERE"
-//Variable represents apiKey.
+//Variable represents the file path which will be attached to an OpsGenie Alert.
 var PathToFile = "/your/path/to/file/here"
-//Variable represents apiKey.
+//Variable represents users will be notified for an OpsGenie Alert.
 var NotifyArr = []string{"YOUR USERNAME HERE"}
-//Variable represents apiKey.
+//Variable represents source of an action of an OpsGenie Alert.
 var Source = "Go API"
-//Variable represents apiKey.
+//Variable represents the list of custom actions of an OpsGenie Alert.
 var Actions = []string{"ping", "pong"}
-//Variable represents apiKey.
+//Variable represents the custom action which will be executed on an OpsGenie Alert.
 var ActionToExec = "pong"
-//Variable represents apiKey.
+//Variable represents the owner of the actions executed at OpsGenie.
 var User = "YOUR USERNAME HERE"
-//Variable represents apiKey.
+//Variable represents an OpsGenie Policy.
 var PolicyName = "YOUR POLICY NAME HERE"
-//Variable represents apiKey.
+//Variable represents an OpsGenie Integration.
 var IntegrationName = "YOUR INTEGRATION NAME HERE"
