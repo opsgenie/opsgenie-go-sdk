3826592738ac357b21b9e346ec6ce3b0dd3bf903
shalom <shalom@outbrain.com>
2016-05-03 15:23:03 -0400

    an...

--- client/opsgenie_team_client.go
+++ client/opsgenie_team_client.go
@@ -116,7 +116,7 @@ func (cli *OpsGenieTeamClient) Get(req team.GetTeamRequest) (*team.GetTeamRespon
 
 // List method retrieves teams from OpsGenie.
 func (cli *OpsGenieTeamClient) List(req team.ListTeamsRequest) (*team.ListTeamsResponse, error) {
-	req.apiKey = cli.apiKey
+	req.APIKey = cli.apiKey
 	resp, err := cli.sendRequest(cli.buildGetRequest(teamURL,req))
 	if resp == nil {
 		return nil, errors.New(err.Error())
