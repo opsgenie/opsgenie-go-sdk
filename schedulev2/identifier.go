fc1bbe80811ab000a3f687ebffb9b1535c07e1e3
vlamug <vlamug@gmail.com>
2017-09-19 12:40:31 +0300

    [GT-2] The fixing of schedule identifier.

--- schedulev2/identifier.go
+++ schedulev2/identifier.go
@@ -12,11 +12,13 @@ type Identifier struct {
 func (request *Identifier) GenerateUrl() (string, url.Values, error) {
 	baseUrl := "/v2/schedules/"
 
-	if request != nil && request.ID != "" {
-		return baseUrl + request.ID, url.Values{}, nil
+	if request != nil {
+		if len(request.ID) != 0 {
+			baseUrl += request.ID
+		} else if len(request.Name) != 0 {
+			baseUrl += request.Name
+		}
 	}
 
-	params := url.Values{}
-
-	return baseUrl, params, nil
+	return baseUrl, url.Values{}, nil
 }
