c1f6546e1a1a5ef66ac2e7ed70e05e03a4c3c28f
vlamug <vlamug@gmail.com>
2017-09-21 12:04:02 +0300

    [GT-2] For policy identifier the Name field was added.
    It means, that it is possible to use name for getting policy.

--- policyv1/identifier.go
+++ policyv1/identifier.go
@@ -6,19 +6,22 @@ import (
 
 const (
 	DisableAction = "disable"
-	EnableAction = "enable"
+	EnableAction  = "enable"
 )
 
 type Identifier struct {
 	ID           string `json:"-"`
-	Alias        string `json:"-"`
-	TinyID       string `json:"-"`
+	Name         string `json:"name"`
 	StatusAction string `json:"-"`
 }
 
 func (request *Identifier) GenerateUrl() (string, url.Values, error) {
 	baseUrl := "/v1/policies"
 	if request != nil {
+		if len(request.Name) != 0 {
+			baseUrl += "/" + request.Name
+		}
+
 		if len(request.ID) != 0 {
 			baseUrl += "/" + request.ID
 		}
@@ -27,7 +30,6 @@ func (request *Identifier) GenerateUrl() (string, url.Values, error) {
 			baseUrl += "/" + request.StatusAction
 		}
 	}
-
 
 	return baseUrl, nil, nil
 }
